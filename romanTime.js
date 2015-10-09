2f5937e8abfcf0f686c6e5f8f8e9ac12583ae0dd
Иванов Дмитрий <darkrogue94@mail.ru>
2015-10-09 14:46:13 +0500

    fixed version

--- romanTime.js
+++ romanTime.js
@@ -1,70 +1,20 @@
-var array=[
-['()      ()','()()    ()','()  ()  ()','()    ()()','()      ()'],
- ['()','()','()','()','()'],
-  ['()      ()','()      ()','  ()    ()','  ()  ()','    ()'],
-  ['()      ()',' ()  ()','    ()','  ()  ()','()      ()'],
-  ['()','()','()','()','()()()()()']];
-var colon=['      ','  ()  ','      ','  ()  ','      '];
 var hours = process.argv[2];
 var minutes = process.argv[3];
 var romanHours = ArabicToRoman(hours);
 var romanMinutes = ArabicToRoman(minutes);
-console.log(romanHours+' : '+romanMinutes);
-
-
-/*for (var i=1; i <= 5; i++)
-    {
-        for (var j=1; j <= romanHours.length+romanMinutes+1; j++)
-            {
-                if (j<=romanHours.length)
-                    {
-                        console.log(i,j);
-                        letterOutput = findLetter(romanHours[j]);
-                        console.log(romanHours[j]);
-                        console.log(letterOutput);
-                    }
-                if (j==(romanHours.length + 1))
-                    {
-                        console.log('11');
-                        console.log(colon[i-1]);
-                    }
-                if (j>(romanHours.length+1))
-                    {
-                        letterOutput = findLetter(romanMinutes[j]);
-                        console.log(romanMinutes[j]);
-                        console.log(letterOutput);
-                    }
-            }    
-    }
-function findLetter(letter)
-{
-    if (letter = 'L')
-        {
-            return array[4][i-1];
-        }
-    if (letter = 'X')
-        {
-            return array[3][i-1];
-        }
-    if (letter = 'V')
-        {
-            return array[2][i-1];
-        }
-    if (letter = 'I')
-        {
-            return array[1][i-1];
-        }
-    if (letter = 'N')
-        {
-            return array[0][i-1];
-        }
-}
-*/
+if (hours>=24 || hours<0 || minutes>59 || minutes<0)
+	{
+		console.log('Время указано не верно');
+	}
+	else
+		{
+			console.log(romanHours+' : '+romanMinutes);
+		}
 
 function ArabicToRoman(arabicValue)
     {
         var romanValue='';
-        if (arabicValue==0)
+        if (arabicValue==='0')
             {
                 romanValue+='N';
             }
@@ -83,7 +33,7 @@ function ArabicToRoman(arabicValue)
                 romanValue+=('X');
                 arabicValue-=10;
             }
-        if (arabicValue===9)
+        if (arabicValue==9)
             {
                 romanValue+=('IX');
                 arabicValue-=9;
@@ -93,7 +43,7 @@ function ArabicToRoman(arabicValue)
                 romanValue+=('V');
                 arabicValue-=5;
             }
-        if (arabicValue===4)
+        if (arabicValue==4)
             {
                 romanValue+=('IV');
                 arabicValue-=4;
